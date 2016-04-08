818c413061d39c287aa4b5b5fe8b8f304cb3716b
siduss <patrizio.antonini@gmail.com>
2016-04-08 03:32:22 +0100

    Update Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -4,28 +4,50 @@ import java.util.List;
 
 public class Pizza {
 	private String name;
-	private List<Product> list_products;
+	private List<Product> products;
+	private double customerPrice;
 	private String info;
+	private String allergenInfo;
+	private boolean isVegetarian;
+	private boolean isMeat;
 
-	public Pizza(String np, List<Product> li) {
-		this.name = np;
-		this.list_products = li;
-		if (checkIfIsWhite(li)) {
-			this.name = this.name + " (NO TOMATO SAUCE)";
+	public Pizza(String pizzaName, List<Product> products) {
+		this.name = pizzaName;
+		this.products = products;
+		if (pizzaName.equals("Meat")) {
+			this.isMeat = true;
+			this.isVegetarian = false;
+		} else if (pizzaName.equals("Vegetarian")) {
+			this.isVegetarian = true;
+			this.isMeat = false;
+		} else {
+			this.isVegetarian = false;
+			this.isMeat = false;
 		}
+		// if (checkIfIsWithoutTomatoSauce(products)) {
+		// this.name = this.name + " (NO TOMATO)";
+		// }
 	}
 
+	public double getPrice() {
+		double price = 0;
+		List<Product> lp = this.products;
+		for (Product p : lp) {
+			price = price + p.getPrice();
+		}
+		return price;
+	}
 
-	private boolean checkIfIsWhite(List<Product> products) {
-		for (Product p : products){
-			if (p.getName().equals("Tomato Sauce")) return false;
-			
+	private boolean checkIfIsWithoutTomatoSauce(List<Product> products) {
+		for (Product p : products) {
+			if (p.getName().equals("Tomato Sauce"))
+				return false;
+
 		}
 		return true;
 	}
 
-
-	public String getNome() {
+	public String getName() {
 		return name;
 	}
 
@@ -33,12 +55,12 @@ public class Pizza {
 		this.name = name;
 	}
 
-	public List<Product> getListProducts() {
-		return list_products;
+	public List<Product> getProducts() {
+		return products;
 	}
 
-	public void setListProducts(List<Product> lista_ingredieti) {
-		this.list_products = lista_ingredieti;
+	public void setProducts(List<Product> products) {
+		this.products = products;
 	}
 
 	public String getInfo() {
@@ -47,6 +69,40 @@ public class Pizza {
 
 	public void setInfo(String info) {
 		this.info = info;
+	}
+
+	public double getCustomerPrice() {
+		return customerPrice;
+	}
+
+	public void setCustomerPrice(double customerPrice) {
+		this.customerPrice = customerPrice;
+	}
+
+	public double getProfit() {
+		return ((this.customerPrice - getPrice()) * 100) / this.customerPrice;
+	}
+
+	public Pizza addProduct(Product product) {
+		List<Product> lp = this.products;
+		if (!checkIfProductExsist(lp, product)) {
+			lp.add(product);
+			return this;
+		}
+		return null;
+	}
+
+	public Pizza deleteProduct(Product product) {
+		List<Product> lp = this.products;
+		if (checkIfProductExsist(lp, product)) {
+			lp.remove(product);
+			return this;
+		}
+		return null;
+	}
+
+	private boolean checkIfProductExsist(List<Product> lp, Product product) {
+		return lp.contains(product);
 	}
 
 }
